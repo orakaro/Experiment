75e5ae64c0a2de21ec9f0cf33adeed6c2c02c57f
orakaro <orakaro@mercari.com>
2017-10-19 20:09:23 +0900

    Sample project

--- Experiment/ViewController.swift
+++ Experiment/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    private let dataSource = DataSource()
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
+        let upstreamData: Driver<[Int]> = Driver.of([1,2,3,4,5,6,7,8,9])
+        
+        upstreamData
+            .drive(tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    class DataSource: NSObject, RxTableViewDataSourceType, UITableViewDataSource, UITableViewDelegate {
+        
+        typealias Element = [Int]
+        var data: [Int] = []
+        
+        func tableView(_ tableView: UITableView, observedEvent: Event<[Int]>) {
+            if case .next(let element) = observedEvent {
+                data = element
+                tableView.reloadData()
+            }
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return data.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomCell
+            cell.label.text = String(data[indexPath.row])
+            return cell
+        }
+        
     }
+}
 
-
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
 }
 
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'Experiment' do
+  use_frameworks!
+
+  pod 'RxSwift', :git => 'https://github.com/ReactiveX/RxSwift', :tag => '4.0.0'
+  pod 'RxCocoa', :git => 'https://github.com/ReactiveX/RxSwift', :tag => '4.0.0'
+
+end
